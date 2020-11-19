c97db77a423614c8db6aaa631d7535d6aa092090
YMGYM <good_day1234@naver.com>
2020-11-19 22:43:27 +0900

    fix

--- db/migrate/20201104065608_create_tasks.rb
+++ db/migrate/20201104065608_create_tasks.rb
@@ -3,6 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :title
       t.datetime :dueDate
+      t.references :user
       t.boolean :finish
       t.timestamps
     end
