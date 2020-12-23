e0b29d142ce2bba05b9fd28a53c865af06dc47fa
YMGYM <good_day1234@naver.com>
2020-12-23 22:37:36 +0900

    add devise token auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_132533) do
+ActiveRecord::Schema.define(version: 2020_12_22_094603) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -23,16 +23,41 @@ ActiveRecord::Schema.define(version: 2020_11_19_132533) do
     t.string "title"
     t.datetime "dueDate"
     t.integer "user_id"
-    t.boolean "finish"
+    t.boolean "finish", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.text "tokens"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
   create_table "works", force: :cascade do |t|
     t.string "title"
     t.datetime "dueDate"
-    t.boolean "finish"
+    t.boolean "finish", default: false
     t.integer "task_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
